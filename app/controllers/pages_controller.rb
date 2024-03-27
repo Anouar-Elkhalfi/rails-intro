ed17a1ca3a9cbd14a187ef0840cd107516c3759a
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-03-27 19:13:36 +0100

    rails new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,8 @@ class PagesController < ApplicationController
 
   def contact
     @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
